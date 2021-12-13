f04aadd982a53076cb5efed688b2186eccaf5f49
Maxime Beugnet <maxime.beugnet@gmail.com>
2021-12-14 00:37:43 +0100

    pb 10 solved with style

--- 10/part1.py
+++ 10/part1.py
@@ -1,25 +1,13 @@
+from re import sub
+
 f = open('inputs/input1.txt', 'r')
 lines = [x.strip() for x in f.readlines()]
 score = 0
 for l in lines:
-    for _ in range(1000):
-        l = l.replace('()', '')
-        l = l.replace('<>', '')
-        l = l.replace('{}', '')
-        l = l.replace('[]', '')
-    for _ in range(1000):
-        l = l.replace('(', '')
-        l = l.replace('<', '')
-        l = l.replace('{', '')
-        l = l.replace('[', '')
+    for _ in range(20):
+        l = sub(r'\(\)|\[]|{}|<>', '', l)
+    for _ in range(20):
+        l = sub(r'[(\[{<]', '', l)
     if len(l) != 0:
-        x = l[0]
-        if x == ')':
-            score += 3
-        if x == ']':
-            score += 57
-        if x == '}':
-            score += 1197
-        if x == '>':
-            score += 25137
+        score += {')': 3, ']': 57, '}': 1197, '>': 25137}.get(l[0])
 print('Score', score)
--- 10/part2.py
+++ 10/part2.py
@@ -1,14 +1,14 @@
-f = open('inputs/input2.txt', 'r')
-lines = [x.strip() for x in f.readlines()]
-score = 0
-for l in lines:
-    for _ in range(1000):
-        l = l.replace('()', '')
-        l = l.replace('<>', '')
-        l = l.replace('{}', '')
-        l = l.replace('[]', '')
-    if not any(x in [')', ']', '}', '>'] for x in l):
-        for c in reversed(l):
-            
+from re import sub
 
-print('Score', score)
+f = open('inputs/input1.txt', 'r')
+lines = [x.strip() for x in f.readlines()]
+scores = []
+for l in lines:
+    for _ in range(20):  # 11 would be enough but magic number is magic
+        l = sub(r'\(\)|\[]|{}|<>', '', l)
+    if not any(x in [')', ']', '}', '>'] for x in l):
+        score = 0
+        for c in reversed(l):
+            score = score * 5 + {'(': 1, '[': 2, '{': 3, '<': 4}.get(c)
+        scores.append(score)
+print('Score', sorted(scores)[len(scores) // 2])
